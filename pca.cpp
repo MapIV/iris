6dce88338eed21b0f5517c92ea6199939511c6d2
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2020-01-23 19:48:46 +0900

    calculate 'Local Point Distribution' in minimul example

--- pca.cpp
+++ pca.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <pcl/common/generate.h>
 #include <pcl/common/pca.h>
@@ -17,15 +18,20 @@ Eigen::Matrix3f correctRotationMatrix(const Eigen::Matrix3f& R)
   }
 }
 
+Eigen::Vector3f correctSigma(const Eigen::Vector3f& sigma)
+{
+  return Eigen::Vector3f(std::abs(sigma.x()), std::abs(sigma.y()), std::abs(sigma.z()));
+}
+
 int main(int argc, char** argv)
 {
   // struct cloud generator
-  float mean = 0;
+  float mean_x = 5, mean_y = 10, mean_z = 0;
   pcl::common::CloudGenerator<pcl::PointXYZ, pcl::common::NormalGenerator<float>> generator;
   std::uint32_t seed = static_cast<std::uint32_t>(time(nullptr));
-  pcl::common::NormalGenerator<float>::Parameters x_params(mean, 1, seed++);
-  pcl::common::NormalGenerator<float>::Parameters y_params(mean, 2, seed++);
-  pcl::common::NormalGenerator<float>::Parameters z_params(mean, 0, seed++);
+  pcl::common::NormalGenerator<float>::Parameters x_params(mean_x, 1, seed++);
+  pcl::common::NormalGenerator<float>::Parameters y_params(mean_y, 2, seed++);
+  pcl::common::NormalGenerator<float>::Parameters z_params(mean_z, 0, seed++);
   generator.setParametersForX(x_params);
   generator.setParametersForY(y_params);
   generator.setParametersForZ(z_params);
@@ -44,7 +50,7 @@ int main(int argc, char** argv)
   // transform
   {
     Eigen::Matrix3f R;
-    R = Eigen::AngleAxisf(static_cast<float>(M_PI / 6.0), Eigen::Vector3f(0, 1, 0));  // Rotate CCW 90[deg] around y-axis
+    R = Eigen::AngleAxisf(static_cast<float>(M_PI / 6.0), Eigen::Vector3f(0, 0, 1));  // Rotate CCW 90[deg] around z-axis
     Eigen::Matrix4f T = Eigen::Matrix4f::Identity();
     T.topLeftCorner(3, 3) = R;
     pcl::transformPointCloud(cloud, cloud, T);
@@ -55,12 +61,22 @@ int main(int argc, char** argv)
     pcl::PCA<pcl::PointXYZ> pca;
     pca.setInputCloud(cloud.makeShared());
     Eigen::Matrix3f R = correctRotationMatrix(pca.getEigenVectors());
-    Eigen::Vector3f sigma = pca.getEigenValues() / N;
+    Eigen::Vector3f sigma = correctSigma(pca.getEigenValues() / N);
     sigma = sigma.array().sqrt();
 
-    std::cout << R << std::endl;
-    std::cout << "" << std::endl;
-    std::cout << sigma.transpose() << std::endl;
+    std::cout << "\n"
+              << R
+              << "\n\n"
+              << sigma.transpose() << std::endl;
+  }
+
+  // compute centroid
+  {
+    pcl::PointXYZ point;
+    pcl::computeCentroid(cloud, point);
+    Eigen::Vector3f center = point.getArray3fMap();
+    std::cout << "\n"
+              << center.transpose() << std::endl;
   }
 
   pcl::io::savePCDFileBinary("random.pcd", cloud);
