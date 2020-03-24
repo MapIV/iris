3b1c9ba0b47f743b7d96c87b4f43590a4193a087
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2020-03-24 17:24:13 +0900

    enclose pcl::CorespondenceEstimator

--- include/core/types.hpp
+++ include/core/types.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/registration/correspondence_estimation_backprojection.h>
+// #include <pcl/registration/correspondence_estimation_backprojection.h>
+#include "estimator/correspondences_estiation_backprojection.hpp"
 #include <pcl/registration/correspondence_rejection_distance.h>
 
 namespace vllm
