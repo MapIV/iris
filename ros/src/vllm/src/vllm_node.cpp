2d368ab658b0225b61cb36090cbde7046b7cdf01
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2020-03-25 20:56:05 +0900

    we can build with catkin_make

--- ros/src/vllm/src/vllm_node.cpp
+++ ros/src/vllm/src/vllm_node.cpp
@@ -6,106 +6,61 @@
 #include <opencv2/opencv.hpp>
 #include <popl.hpp>
 
-void mono_tracking(
-    // const std::shared_ptr<openvslam::config>& cfg, const std::string& vocab_file_path,
-    //   const std::string& mask_img_path, const bool eval_log, const std::string& map_db_path
-)
-{
-  //   // load the mask image
-  //   const cv::Mat mask = mask_img_path.empty() ? cv::Mat{} : cv::imread(mask_img_path, cv::IMREAD_GRAYSCALE);
-
-  //   // build a SLAM system
-  //   openvslam::system SLAM(cfg, vocab_file_path);
-  //   // startup the SLAM process
-  //   SLAM.startup();
-
-  //   // create a viewer object
-  //   // and pass the frame_publisher and the map_publisher
-  // #ifdef USE_PANGOLIN_VIEWER
-  //   pangolin_viewer::viewer viewer(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
-  // #elif USE_SOCKET_PUBLISHER
-  //   socket_publisher::publisher publisher(cfg, &SLAM, SLAM.get_frame_publisher(), SLAM.get_map_publisher());
-  // #endif
-
-  //   std::vector<double> track_times;
-  //   const auto tp_0 = std::chrono::steady_clock::now();
-
-  //   // initialize this node
-  //   const ros::NodeHandle nh;
-  //   image_transport::ImageTransport it(nh);
-
-  //   // run the SLAM as subscriber
-  //   image_transport::Subscriber sub = it.subscribe("camera/image_raw", 1, [&](const sensor_msgs::ImageConstPtr& msg) {
-  //     const auto tp_1 = std::chrono::steady_clock::now();
-  //     const auto timestamp = std::chrono::duration_cast<std::chrono::duration<double>>(tp_1 - tp_0).count();
-
-  //     // input the current frame and estimate the camera pose
-  //     SLAM.feed_monocular_frame(cv_bridge::toCvShare(msg, "bgr8")->image, timestamp, mask);
-
-  //     const auto tp_2 = std::chrono::steady_clock::now();
-
-  //     const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
-  //     track_times.push_back(track_time);
-  //   });
-
-  // run the viewer in another thread
-  // #ifdef USE_PANGOLIN_VIEWER
-  //   std::thread thread([&]() {
-  //     viewer.run();
-  //     if (SLAM.terminate_is_requested()) {
-  //       // wait until the loop BA is finished
-  //       while (SLAM.loop_BA_is_running()) {
-  //         std::this_thread::sleep_for(std::chrono::microseconds(5000));
-  //       }
-  //       ros::shutdown();
-  //     }
-  //   });
-
-  ros::spin();
-
-  // automatically close the viewer
-}
-
 int main(int argc, char* argv[])
 {
-  ros::init(argc, argv, "vllm_node");
+  // analyze arugments
+  popl::OptionParser op("Allowed options");
+  auto config_file_path = op.add<popl::Value<std::string>>("c", "config", "config file path");
+  try {
+    op.parse(argc, argv);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  if (!config_file_path->is_set()) {
+    std::cerr << "invalid arguments" << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
-  // // check validness of options
-  // if (help->is_set()) {
-  //   std::cerr << op << std::endl;
-  //   return EXIT_FAILURE;
-  // }
-  // if (!vocab_file_path->is_set() || !setting_file_path->is_set()) {
-  //   std::cerr << "invalid arguments" << std::endl;
-  //   std::cerr << std::endl;
-  //   std::cerr << op << std::endl;
-  //   return EXIT_FAILURE;
-  // }
+  // Initialize config
+  vllm::Config config(config_file_path->value());
 
-  // // setup logger
-  // spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] %^[%L] %v%$");
-  // if (debug_mode->is_set()) {
-  //   spdlog::set_level(spdlog::level::debug);
-  // } else {
-  //   spdlog::set_level(spdlog::level::info);
-  // }
+  // Load LiDAR map
+  vllm::map::Parameter map_param(
+      config.pcd_file, config.voxel_grid_leaf, config.normal_search_leaf, config.submap_grid_leaf);
+  std::shared_ptr<vllm::map::Map> map = std::make_shared<vllm::map::Map>(map_param);
 
-  // // load configuration
-  // std::shared_ptr<openvslam::config> cfg;
-  // try {
-  //   cfg = std::make_shared<openvslam::config>(setting_file_path->value());
-  // } catch (const std::exception& e) {
-  //   std::cerr << e.what() << std::endl;
-  //   return EXIT_FAILURE;
-  // }
+  // Initialize system
+  std::shared_ptr<vllm::System> system = std::make_shared<vllm::System>(config, map);
+
+  // Initialize viewer
+  vllm::viewer::PangolinViewer pangolin_viewer(system);
+  pangolin_viewer.startLoop();
+  cv::namedWindow("VLLM", cv::WINDOW_AUTOSIZE);
 
 
-  // // run tracking
-  // if (cfg->camera_->setup_type_ == openvslam::camera::setup_type_t::Monocular) {
-  //   mono_tracking(cfg, vocab_file_path->value(), mask_img_path->value(), eval_log->is_set(), map_db_path->value());
-  // } else {
-  //   throw std::runtime_error("Invalid setup type: " + cfg->camera_->get_setup_type_string());
-  // }
+  bool loop = true;
+  std::chrono::system_clock::time_point m_start;
 
-  return EXIT_SUCCESS;
+  while (loop) {
+    m_start = std::chrono::system_clock::now();
+
+    system->execute();
+
+    std::cout << "time= \033[35m"
+              << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - m_start).count()
+              << "\033[m ms" << std::endl;
+
+    // visualize by OpenCV
+    cv::imshow("VLLM", system->getFrame());
+    int key = cv::waitKey(1);
+    if (key == 'q') break;
+    if (key == 's') {
+      while (key == 's')
+        key = cv::waitKey(0);
+    }
+  }
+
+  pangolin_viewer.quitLoop();
+  return 0;
 }
