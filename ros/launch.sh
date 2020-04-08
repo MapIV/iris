4626567d69956bf41f5d7c473202165256005ed0
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2020-04-08 17:02:44 +0900

    make launch.sh better

--- ros/launch.sh
+++ ros/launch.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "指定された引数は$#個です。"
-  echo "実行するには1個の引数が必要です。"
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+EXECUTE_DIR=$(pwd)
+
+# Since the path of the pcd file is written in the config.yaml,
+# the file cannot be opened unless this scripts is called from an appropriate location. 
+if [ ${SCRIPT_DIR} != ${EXECUTE_DIR} ]; then
+  echo "Please call this script from ${SCRIPT_DIR}"
   exit 1
 fi
 
+# analyze the arguments
+if [ $# -ne 1 ]; then
+  echo "This script requires 1 arguments."
+  echo "You set $# arguments"
+  echo "(ex) ./launch.sh ./moriyama.pcd"
+  exit 1
+fi
 
+# manage `roscore`
 if pgrep -x "roscore" > /dev/null
 then
   echo "roscore is running"
 else
   echo "Because roscore is stopping, it starts roscore"
   roscore &
+  sleep 3
 fi
 
-./devel/lib/vllm/vllm_node -c $1
+# start rviz
+#rosrun rviz rviz &
+
+# execution
+${SCRIPT_DIR}/devel/lib/vllm/vllm_node -c $1
