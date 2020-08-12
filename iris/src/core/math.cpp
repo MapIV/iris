a8df458f27070a2864963239da8e1198aa55022a
Kento Yabuuchi <moc.liamg.8y8@gmail.com>
2020-08-12 14:42:36 +0900

    add Copyright to all [ch]pp

--- iris/src/core/math.cpp
+++ iris/src/core/math.cpp
@@ -1,9 +1,38 @@
+// Copyright (c) 2020, Map IV, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the Map IV, Inc. nor the names of its contributors
+//   may be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #include "core/math.hpp"
 
 namespace iris
 {
 namespace so3
 {
+namespace
+{
+constexpr float EPSILON = 1e-7f;
+}
 Eigen::Matrix3f hat(const Eigen::Vector3f& xi)
 {
   Eigen::Matrix3f S;
@@ -19,7 +48,7 @@ Eigen::Vector3f log(const Eigen::Matrix3f& R)
 {
   Eigen::Vector3f xi = Eigen::Vector3f::Zero();
   float w_length = static_cast<float>(std::acos((R.trace() - 1.0f) * 0.5f));
-  if (w_length > 1e-6f) {
+  if (w_length > EPSILON) {
     Eigen::Vector3f tmp;
     tmp << R(2, 1) - R(1, 2), R(0, 2) - R(2, 0), R(1, 0) - R(0, 1);
     xi = 1.0f / (2.0f * static_cast<float>(std::sin(w_length))) * tmp * w_length;
--- iris/src/map/info.hpp
+++ iris/src/map/info.hpp
@@ -1,3 +1,28 @@
+// Copyright (c) 2020, Map IV, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name of the Map IV, Inc. nor the names of its contributors
+//   may be used to endorse or promote products derived from this software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #pragma once
 #include <Eigen/Dense>
 
@@ -24,12 +49,12 @@ struct Info {
 
   bool isEqual(const Info& a, const Info& b) const
   {
-    const float epsilon = 1e-6f;
-    if (std::fabs(a.x - b.x) > epsilon)
+    constexpr float EPSILON = 1e-7f;
+    if (std::fabs(a.x - b.x) > EPSILON)
       return false;
-    if (std::fabs(a.y - b.y) > epsilon)
+    if (std::fabs(a.y - b.y) > EPSILOn)
       return false;
-    if (std::fabs(a.theta - b.theta) > epsilon)
+    if (std::fabs(a.theta - b.theta) > EPSILON)
       return false;
     return true;
   }
